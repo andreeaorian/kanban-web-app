5b0ed66cc8ebe2a90c95d11a4484e75715b9500a
andreea-orian <andreea.orian@dotwhite.com>
2023-07-20 16:18:50 +0300

    Move DndProvider at App level

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,40 +1,32 @@
 import { useSelector } from "react-redux";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { TouchBackend } from "react-dnd-touch-backend";
 import { RootState } from "../../redux/store";
 import BoardColumn from "./components/board-column";
 import Header from "../header/header";
 
 import "./board.scss";
-import { useTouchDevice } from "../../hooks/use-touch-device";
 
 export default function Board() {
 	const selectedBoard = useSelector((state: RootState) =>
 		state.app.boards.find((b) => b.isSelected)
 	);
-	const isTouchDevice = useTouchDevice();
-	const backendForDndProvider = isTouchDevice ? TouchBackend : HTML5Backend;
 
 	return (
-		<DndProvider backend={backendForDndProvider}>
-			<div className="board">
-				<Header />
-				<div className="board-content">
-					{selectedBoard?.columns.map((column) => {
-						return (
-							<BoardColumn
-								columnName={column.title}
-								color={column.color}
-								key={column.title}
-								tasks={selectedBoard.tasks.filter(
-									(t) => t.status === column.title
-								)}
-							/>
-						);
-					})}
-				</div>
+		<div className="board">
+			<Header />
+			<div className="board-content">
+				{selectedBoard?.columns.map((column) => {
+					return (
+						<BoardColumn
+							columnName={column.title}
+							color={column.color}
+							key={column.title}
+							tasks={selectedBoard.tasks.filter(
+								(t) => t.status === column.title
+							)}
+						/>
+					);
+				})}
 			</div>
-		</DndProvider>
+		</div>
 	);
 }
